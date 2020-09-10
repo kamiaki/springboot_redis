95329119d9f4332c345aa2e2024c0f01ecba6bb4
kamiaki <247557768@qq.com>
2020-09-10 11:22:42 +0800

    增加线程安全连接池

--- src/main/java/com/aki/redis/service/serviceimpl/RedisService.java
+++ src/main/java/com/aki/redis/service/serviceimpl/RedisService.java
@@ -1,7 +1,6 @@
 package com.aki.redis.service.serviceimpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
@@ -16,7 +15,7 @@ import javax.annotation.Resource;
 public class RedisService {
 
     //用来处理字符串
-     @Resource(name = "stringRedisTemplate")
+    @Resource(name = "stringRedisTemplate")
     ValueOperations<String, String> valOpsStr;
     @Autowired
     StringRedisTemplate stringRedisTemplate;
@@ -31,6 +30,7 @@ public class RedisService {
 
     /**
      * 根据指定key获取String
+     *
      * @param key
      * @return
      */
@@ -42,6 +42,7 @@ public class RedisService {
 
     /**
      * 设置Str缓存
+     *
      * @param key
      * @param val
      */
@@ -52,6 +53,7 @@ public class RedisService {
 
     /**
      * 删除指定key
+     *
      * @param key
      */
     public void del(String key) {
@@ -61,16 +63,18 @@ public class RedisService {
 
     /**
      * 设置obj缓存
+     *
      * @param o1
      * @param o2
      */
     public void setObj(Object o1, Object o2) {
-        redisTemplate.opsForValue().set("objCopy",o2);//另一种写法
+        redisTemplate.opsForValue().set("objCopy", o2);//另一种写法
         valOpsObj.set(o1, o2);
     }
 
     /**
      * 根据指定o获取Object
+     *
      * @param o
      * @return
      */
@@ -82,6 +86,7 @@ public class RedisService {
 
     /**
      * 删除Obj缓存
+     *
      * @param o
      */
     public void delObj(Object o) {
