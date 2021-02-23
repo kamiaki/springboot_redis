612c25360987aa92fc1f101ab4d337aa835c9325
kamiaki <247557768@qq.com>
2021-02-23 14:48:24 +0800

    分布式锁

--- src/main/java/com/aki/redis/controller/Lock.java
+++ src/main/java/com/aki/redis/controller/Lock.java
@@ -75,4 +75,45 @@ public class Lock {
         }
         return "拿到锁，执行完了";
     }
+
+    /**
+     * 集群实现原理，就是好多个锁同意判断，用RedissonRedLock，
+     * 需要3/2+1=2，即至少2个sentinel集群响应成功，就是说一半以上上锁了，就是上锁了
+     *
+     * Config config1 = new Config();
+     * config1.useSingleServer().setAddress("redis://172.29.1.180:5378")
+     *         .setPassword("a123456").setDatabase(0);
+     * RedissonClient redissonClient1 = Redisson.create(config1);
+     *
+     * Config config2 = new Config();
+     * config2.useSingleServer().setAddress("redis://172.29.1.180:5379")
+     *         .setPassword("a123456").setDatabase(0);
+     * RedissonClient redissonClient2 = Redisson.create(config2);
+     *
+     * Config config3 = new Config();
+     * config3.useSingleServer().setAddress("redis://172.29.1.180:5380")
+     *         .setPassword("a123456").setDatabase(0);
+     * RedissonClient redissonClient3 = Redisson.create(config3);
+     *
+     * String resourceName = "REDLOCK";
+     * RLock lock1 = redissonClient1.getLock(resourceName);
+     * RLock lock2 = redissonClient2.getLock(resourceName);
+     * RLock lock3 = redissonClient3.getLock(resourceName);
+     *
+     * RedissonRedLock redLock = new RedissonRedLock(lock1, lock2, lock3);
+     * boolean isLock;
+     * try {
+     *     isLock = redLock.tryLock(500, 30000, TimeUnit.MILLISECONDS);
+     *     System.out.println("isLock = "+isLock);
+     *     if (isLock) {
+     *         //TODO if get lock success, do something;
+     *         Thread.sleep(30000);
+     *     }
+     * } catch (Exception e) {
+     * } finally {
+     *     // 无论如何, 最后都要解锁
+     *     System.out.println("");
+     *     redLock.unlock();
+     * }
+     */
 }
